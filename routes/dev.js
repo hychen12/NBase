c4fb87c8e2f6c6cced869afa39a4ecfe80a63eb8
mllee <min.lung.lee@gmail.com>
2016-05-20 17:38:14 +0800

    init

--- routes/dev.js
+++ routes/dev.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var dao = require('../lib/dao.js');
 
+
+router.use(require('../lib/loginChecker.js'));
+
 router.get('/', function (req, res, next) {
 	res.render('dev/_', {layout: '_layout/default'});
 });
@@ -37,6 +40,13 @@ router.post('/db/ajax/delete', function (req, res, next) {
 	var bulk = req.body.bulk;
 	dao[bulk ? 'bulkDelete' : 'delete'](sql, [],
 		function(result){ res.json(result); },
+		function(error){ res.status(500).end(error.message); });
+});
+
+router.post('/db/ajax/transaction', function (req, res, next) {
+	var sqls = req.body.sql.split(';');
+	dao.transaction(sqls,
+		function(result){ res.json('Done!'); },
 		function(error){ res.status(500).end(error.message); });
 });
 
